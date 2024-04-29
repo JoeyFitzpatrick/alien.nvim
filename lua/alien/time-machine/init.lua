557ff6312ee8aa5f8a22665590a07a6de4fd5cc1
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-04-28 23:08:38 -0500

    chore: target time machine buffer for delete

--- lua/alien/time-machine/init.lua
+++ lua/alien/time-machine/init.lua
@@ -1,17 +1,17 @@
 local M = {}
 
-M.time_machine_win_num = nil
+M.time_machine_bufnr = nil
 
 M.toggle = function()
-	if M.time_machine_win_num then
-		vim.cmd("bdelete")
-		M.time_machine_win_num = nil
+	if M.time_machine_bufnr then
+		vim.cmd("bdelete " .. M.time_machine_bufnr)
+		M.time_machine_bufnr = nil
 	else
 		local window_width = vim.api.nvim_win_get_width(0)
 		local split_width = math.floor(window_width * 0.25)
 		-- vim.cmd("bo " .. split_height .. " split " .. filename)
 		vim.cmd(split_width .. " vnew")
-		M.time_machine_win_num = vim.api.nvim_get_current_win()
+		M.time_machine_bufnr = vim.api.nvim_get_current_buf()
 	end
 end
 
