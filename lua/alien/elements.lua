5cb8569ecaaaded73df0fc00deb499e5a051cd6b
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-06-16 20:34:20 -0500

    feat: v1 elements

--- lua/alien/elements.lua
+++ lua/alien/elements.lua
@@ -1,29 +1,81 @@
 local M = {}
 
----
----@param action fun(): string[]
----@return string[]
-local run_action = function(action)
-	return action()
-end
+---@alias Object "commit" | "local_file" | "commit_file" | nil
+---@alias Action { get: fun(): string[], object_type: Object }
 
----@param bufnr number
----@param opts table | nil
-local split = function(bufnr, opts)
-	opts = opts or {}
-	vim.cmd.split(opts)
-	vim.api.nvim_win_set_buf(0, bufnr)
-end
-
----@param type "float" | "split" | "tab" | "buffer"
----@param action fun(): string[]
----@param opts table | nil
-M.create = function(type, action, opts)
+---@param get fun(): string[]
+---@return number
+local function create(get)
+	local lines = get()
 	local bufnr = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, run_action(action))
-	if type == "split" then
-		split(bufnr, opts)
-	end
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+	return bufnr
+end
+
+--- Create a new buffer with the given action, and open it in a floating window
+---@param action Action
+---@param opts vim.api.keyset.win_config | nil
+---@return number
+M.float = function(action, opts)
+	local width = math.floor(vim.o.columns * 0.8)
+	local height = math.floor(vim.o.lines * 0.8)
+	local col = math.floor((vim.o.columns - width) / 2)
+	local row = math.floor((vim.o.lines - height) / 2)
+
+	---@type vim.api.keyset.win_config
+	local default_float_opts = {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = col,
+		row = row,
+		style = "minimal",
+	}
+	local float_opts = vim.tbl_extend("force", default_float_opts, opts or {})
+	local bufnr = create(action.get)
+	vim.api.nvim_open_win(bufnr, true, float_opts)
+	return bufnr
+end
+
+--- Create a new buffer with the given action, and open it in a new split
+---@param action Action
+---@param opts vim.api.keyset.win_config | nil
+---@return number
+M.split = function(action, opts)
+	---@type vim.api.keyset.win_config
+	local default_split_opts = {
+		split = "right",
+	}
+	local float_opts = vim.tbl_extend("force", default_split_opts, opts or {})
+	local bufnr = create(action.get)
+	vim.api.nvim_open_win(bufnr, true, float_opts)
+	return bufnr
+end
+
+--- Create a new buffer with the given action, and open it in a new tab
+---@param action Action
+---@param opts { title: string | nil } | nil
+---@return number
+M.tab = function(action, opts)
+	opts = opts or {}
+	local bufnr = create(action.get)
+	vim.api.nvim_buf_set_name(bufnr, opts.title or "Alien")
+	vim.cmd("tabnew")
+	local winnr = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_buf(winnr, bufnr)
+	return bufnr
+end
+
+--- Create a new buffer with the given action, and open it in a target window
+---@param action Action
+---@param opts { winnr: number | nil} | nil
+---@return number
+M.buffer = function(action, opts)
+	local default_buffer_opts = { winnr = vim.api.nvim_get_current_win() }
+	local buffer_opts = vim.tbl_extend("force", default_buffer_opts, opts or {})
+	local bufnr = create(action.get)
+	vim.api.nvim_win_set_buf(buffer_opts.winnr, bufnr)
+	return bufnr
 end
 
 return M
--- lua/alien/init.lua
+++ lua/alien/init.lua
@@ -6,10 +6,36 @@ M.setup = function(opts)
 	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
 end
 
-M.test = function()
-	require("alien.elements").create("split", function()
-		return { M.config.test }
-	end)
+M.float = function()
+	require("alien.elements").float({
+		get = function()
+			return { "item from config", M.config.test }
+		end,
+	}, { border = "rounded", title = "test" })
+end
+
+M.split = function()
+	require("alien.elements").split({
+		get = function()
+			return { "item from config", M.config.test }
+		end,
+	})
+end
+
+M.tab = function()
+	require("alien.elements").tab({
+		get = function()
+			return { "item from config", M.config.test }
+		end,
+	})
+end
+
+M.buffer = function()
+	require("alien.elements").buffer({
+		get = function()
+			return { "item from config", M.config.test }
+		end,
+	})
 end
 
 return M
