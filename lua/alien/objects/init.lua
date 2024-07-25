7b44a733bd30494d3224c6f816765d93c9f57523
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-07-24 21:22:53 -0500

    chore: clean up and test get_object_type

--- lua/alien/objects/init.lua
+++ lua/alien/objects/init.lua
@@ -7,8 +7,14 @@ M.OBJECT_TYPES = {
 	LOCAL_BRANCH = "local_branch",
 	COMMIT_FILE = "commit_file",
 	COMMIT = "commit",
+	DIFF = "diff",
+}
+
+M.GIT_VERBS = {
+	LOG = "log",
 	STATUS = "status",
 	DIFF = "diff",
+	BRANCH = "branch",
 }
 
 ---
@@ -19,14 +25,15 @@ M.get_object_type = function(cmd)
 	if first_word ~= "git" then
 		return nil
 	end
-	local git_verb = cmd:match("%S+%s+(%S+)")
-	if git_verb == M.OBJECT_TYPES.STATUS then
+	local word = cmd:match("%s[^%-]%S+")
+	local git_verb = word:match("%S+")
+	if git_verb == M.GIT_VERBS.STATUS then
 		return M.OBJECT_TYPES.LOCAL_FILE
-	elseif git_verb == "log" then
+	elseif git_verb == M.GIT_VERBS.LOG then
 		return M.OBJECT_TYPES.COMMIT
-	elseif git_verb == "diff" then
+	elseif git_verb == M.GIT_VERBS.DIFF then
 		return M.OBJECT_TYPES.DIFF
-	elseif git_verb == "branch" then
+	elseif git_verb == M.GIT_VERBS.BRANCH then
 		return M.OBJECT_TYPES.LOCAL_BRANCH
 	end
 	return nil
---
+++ tests/objects/get_object_type_spec.lua
@@ -0,0 +1,29 @@
+local objects = require("alien.objects")
+local get_object_type = objects.get_object_type
+local OBJECT_TYPES = objects.OBJECT_TYPES
+local GIT_VERBS = objects.GIT_VERBS
+
+describe("get_object_type", function()
+	it("should return the matching git object for each git verb", function()
+		local verb_object_pairs = {
+			{ verb = GIT_VERBS.STATUS, object = OBJECT_TYPES.LOCAL_FILE },
+			{ verb = GIT_VERBS.LOG, object = OBJECT_TYPES.COMMIT },
+			{ verb = GIT_VERBS.DIFF, object = OBJECT_TYPES.DIFF },
+			{ verb = GIT_VERBS.BRANCH, object = OBJECT_TYPES.LOCAL_BRANCH },
+		}
+		for _, pair in pairs(verb_object_pairs) do
+			assert.equal(pair.object, get_object_type("git " .. pair.verb))
+		end
+	end)
+	it("should return the matching git object when there are flags", function()
+		local verb_object_pairs = {
+			{ verb = GIT_VERBS.STATUS, object = OBJECT_TYPES.LOCAL_FILE },
+			{ verb = GIT_VERBS.LOG, object = OBJECT_TYPES.COMMIT },
+			{ verb = GIT_VERBS.DIFF, object = OBJECT_TYPES.DIFF },
+			{ verb = GIT_VERBS.BRANCH, object = OBJECT_TYPES.LOCAL_BRANCH },
+		}
+		for _, pair in pairs(verb_object_pairs) do
+			assert.equal(pair.object, get_object_type("git -f --flag --flag='something' " .. pair.verb .. " --flag -f"))
+		end
+	end)
+end)
