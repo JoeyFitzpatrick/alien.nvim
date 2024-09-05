286e780c25527f9eafba15a2a84e044f396d78c0
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-09-05 15:28:45 -0500

    feat: commit display strategy

--- lua/alien/command-mode/display-strategies/commit.lua
+++ lua/alien/command-mode/display-strategies/commit.lua
@@ -1,11 +1,35 @@
 local DISPLAY_STRATEGIES = require("alien.command-mode.constants").DISPLAY_STRATEGIES
+local utils = require("alien.command-mode.utils")
 
 local M = {}
+
+local print_options = {
+  "-C",
+  "--reuse-message",
+  "--squash",
+  "--long",
+  "--short",
+  "--porcelain",
+  "-z",
+  "--null",
+  "-F",
+  "--file",
+  "-m",
+  "--message",
+  "--allow-empty",
+  "--allow-message",
+  "--no-edit",
+  "--dry-run",
+}
 
 ---@param cmd string
 ---@return DisplayStrategy
 M.get_strategy = function(cmd)
-  return DISPLAY_STRATEGIES.UI
+  local options = utils.parse_command_options(cmd)
+  if utils.overlap(options, print_options) then
+    return DISPLAY_STRATEGIES.PRINT
+  end
+  return DISPLAY_STRATEGIES.INTERACTIVE
 end
 
 return M
