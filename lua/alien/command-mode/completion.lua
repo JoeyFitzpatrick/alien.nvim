d29c1a7d64fae599dcbd25550fe0048ace95f538
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-09-17 00:40:34 -0500

    feat: local branch completions

--- lua/alien/command-mode/completion.lua
+++ lua/alien/command-mode/completion.lua
@@ -21,6 +21,18 @@ local function get_subcommand(cmdline)
   return words[2]
 end
 
+local function get_arguments(subcommand)
+  local SUBCOMMAND_TO_ARGUMENTS_MAP = {
+    checkout = "git for-each-ref --format='%(refname:short)' refs/heads/",
+    switch = "git for-each-ref --format='%(refname:short)' refs/heads/",
+  }
+  local cmd = SUBCOMMAND_TO_ARGUMENTS_MAP[subcommand]
+  if not cmd then
+    return {}
+  end
+  return vim.fn.systemlist(cmd)
+end
+
 --- takes an arbitrary git command, and returns completion options
 ---@param arglead string
 ---@param cmdline string
@@ -33,8 +45,12 @@ M.complete_git_command = function(arglead, cmdline)
   if space_count == 1 then
     return base_subcommands()
   end
-  if space_count > 1 and arglead:sub(1, 1) == "-" then
-    return subcommand_flags(get_subcommand(cmdline))
+  if space_count > 1 then
+    local subcommand = get_subcommand(cmdline)
+    if arglead:sub(1, 1) == "-" then
+      return subcommand_flags(subcommand)
+    end
+    return get_arguments(subcommand)
   end
   return { arglead, cmdline }
 end
