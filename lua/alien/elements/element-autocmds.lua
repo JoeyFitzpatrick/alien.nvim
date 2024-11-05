ae4cdbbe122e697aebb7bf51b405c7221eef5b23
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-11-04 20:56:12 -0600

    chore: sort porcelain commands and aggregate autocmds

--- lua/alien/elements/element-autocmds.lua
+++ lua/alien/elements/element-autocmds.lua
@@ -2,16 +2,7 @@ local M = {}
 
 local element_group = vim.api.nvim_create_augroup("Alien", { clear = true })
 M.set_element_autocmds = function(bufnr)
-  vim.api.nvim_create_autocmd("BufDelete", {
-    desc = "Deregister element",
-    buffer = bufnr,
-    callback = function()
-      require("alien.elements.register").deregister_element(bufnr)
-    end,
-    group = element_group,
-  })
-
-  vim.api.nvim_create_autocmd("WinClosed", {
+  vim.api.nvim_create_autocmd({ "BufDelete", "WinClosed" }, {
     desc = "Deregister element",
     buffer = bufnr,
     callback = function()
