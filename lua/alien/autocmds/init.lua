afa7b386092b2e8008b97992e7dece8aabbceb85
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-11-10 22:30:01 -0600

    chore: decided not to use object-specific autocmds for now

--- lua/alien/autocmds/init.lua
+++ lua/alien/autocmds/init.lua
@@ -12,18 +12,4 @@ M.set_element_autocmds = function(bufnr)
     })
 end
 
---- Set object-type-specific autocmds
----@param bufnr integer
----@param object_type AlienObject | nil
-M.set_object_autocmds = function(bufnr, object_type)
-    if not object_type then
-        return
-    end
-    local ok, extractor = pcall(require, "alien.extractors." .. object_type:gsub("_", "-") .. "-extractor")
-    if not ok then
-        vim.notify("Alien couldn't figure out which git extractor to use", vim.log.levels.ERROR)
-        return nil
-    end
-end
-
 return M
