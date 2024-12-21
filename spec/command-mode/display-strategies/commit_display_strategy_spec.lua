723a6c643dad1914445db17ab75fcd50827dcc36
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-12-20 22:00:47 -0600

    test: tests for commit display strategy

--- spec/command-mode/display-strategies/commit_display_strategy_spec.lua
+++ spec/command-mode/display-strategies/commit_display_strategy_spec.lua
@@ -1,13 +1,18 @@
--- local get_commit_strategy = require("alien.command-mode.display-strategies.commit").get_strategy
--- local DISPLAY_STRATEGIES = require("alien.command-mode.constants").DISPLAY_STRATEGIES
+local get_commit_strategy = require("alien.command-mode.display-strategies.commit").get_strategy
+local DISPLAY_STRATEGIES = require("alien.command-mode.constants").DISPLAY_STRATEGIES
 
--- describe("get_commit_strategy", function()
---   it("returns 'print' for commands that should print output", function()
---     local result = get_commit_strategy("git commit -m mockmessage")
---     assert.are.equal(result, DISPLAY_STRATEGIES.PRINT)
---   end)
---   it("returns 'interactive-commit' for commands that should use interactive output", function()
---     local result = get_commit_strategy("git commit")
---     assert.are.equal(result, DISPLAY_STRATEGIES.INTERACTIVE_COMMIT)
---   end)
--- end)
+describe("get_commit_strategy", function()
+    it("uses dynamic resize (i.e. the default) for commands that should print output", function()
+        local display_strategy, opts = get_commit_strategy("git commit -m 'mock message'")
+        assert.are.equal(DISPLAY_STRATEGIES.TERMINAL, display_strategy)
+        assert.are.equal(nil, opts)
+    end)
+    it("uses static size for commands that should use interactive output", function()
+        local display_strategy, opts = get_commit_strategy("git commit")
+        if not opts then
+            error("opts should be defined here")
+        end
+        assert.are.equal(DISPLAY_STRATEGIES.TERMINAL, display_strategy)
+        assert.are.equal(false, opts.dynamic_resize)
+    end)
+end)
